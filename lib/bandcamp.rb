092552cfd6e17fa20cb03c520a907a71496766d8
Jason Cale <jase@gofreerange.com>
2011-03-06 20:30:16 +0000

    Added support for tracks and updated to version 2 & 3 of the API which brings the gem up to date with the current API

--- lib/bandcamp.rb
+++ lib/bandcamp.rb
@@ -6,57 +6,125 @@ module Bandcamp
     format :json
     base_uri 'api.bandcamp.com/api'
 
-    class << self; attr_accessor :api_key; end;    
+    class << self
+      attr_writer :api_key
+      
+      def api_key
+        if @api_key.nil?
+          raise 'NoAPIKeyGiven'
+        else
+          return @api_key
+        end
+      end
+      
+      def url(search)
+        get("/url/1/info", :query => { :key => api_key, :url => search })        
+      end
+    end 
   end
   
   class Band < Base
-    attr_reader :name, :url, :band_id, :subdomain
+    attr_reader :name, :url, :band_id, :subdomain, :offsite_url
     
-    def initialize(band_id)
-      raise 'NoAPIKeyGiven' if Bandcamp::Base.api_key.nil?
-      response = self.class.get("/band/1/info", :query => { :key => Bandcamp::Base.api_key, :band_id => band_id })      
-      
-      @name = response['name']
-      @url = response['url']
-      @band_id = response['band_id']
-      @subdomain = response['subdomain']
+    def initialize(band)      
+      @name        = band['name']
+      @url         = band['url']
+      @band_id     = band['band_id']
+      @subdomain   = band['subdomain']
+      @offsite_url = band['offsite_url']
     end
     
     def discography
-      self.class.get("/band/1/discography", :query => { :key => Bandcamp::Base.api_key, :band_id => band_id })['discography']
+      response = self.class.get("/band/3/discography", :query => { :key => Bandcamp::Base.api_key, :band_id => band_id })
+      return response['discography'] if response && response['discography'] 
+    end
+    
+    class << self
+      def find(name)
+        response = get("/band/3/search", :query => { :key => Bandcamp::Base.api_key, :name => name })
+        if response && response['results']
+          response['results'].map { |band| new(band) } 
+        else
+          response
+        end
+      end
+      
+      def load(*band_ids)
+        response = get("/band/3/info", :query => { :key => Bandcamp::Base.api_key, :band_id => band_ids.join(",") })
+        if band_ids.length > 1
+          band_ids.map { |band_id|
+            new(response[band_id.to_s])
+          }
+        else
+          new(response) if response
+        end
+      end
     end
   end
   
   class Album < Base
     attr_reader :title, :release_date, :downloadable, :url, :about, :credits,
-                :small_art_url, :large_art_url, :artist, :album_id, :band_id
+                :small_art_url, :large_art_url, :artist, :album_id, :band_id, :tracks
     
-    def initialize(id)
-      raise 'NoAPIKeyGiven' if Bandcamp::Base.api_key.nil?
-      response = self.class.get("/album/1/info", :query => { :key => Bandcamp::Base.api_key, :album_id => id })
-      
-      @title = response['title']
-      @release_date = Time.at(response['release_date'])
-      @downloadable = response['downloadable']
-      @url = response['url']
-      @about = response['about']
-      @credits = response['credits']      
-      @small_art_url = response['small_art_url']
-      @large_art_url = response['large_art_url']
-      @artist = response['artist']
-      @album_id = response['album_id']
-      @band_id = response['band_id']
+    def initialize(album)      
+      @title         = album['title']
+      @release_date  = Time.at(album['release_date'])
+      @downloadable  = album['downloadable']
+      @url           = album['url']
+      @about         = album['about']
+      @credits       = album['credits']      
+      @small_art_url = album['small_art_url']
+      @large_art_url = album['large_art_url']
+      @artist        = album['artist']
+      @album_id      = album['album_id']
+      @band_id       = album['band_id']
+      @tracks        = album['tracks'].map{ |track| Track.new(track) }
     end
-    
-    def tracks
-      @tracks = []
-    end
-    
+        
     def band
-      @band ||= Band.new(band_id)
+      @band ||= Band.load(band_id)
+    end
+    
+    class << self
+      def load(id)
+        response = get("/album/2/info", :query => { :key => Bandcamp::Base.api_key, :album_id => id })
+        new(response) if response
+      end
     end
   end
   
-  class Track
+  class Track < Base
+    attr_reader :lyrics, :downloadable, :duration, :about, :album_id, :credits,
+                :streaming_url, :number, :title, :url, :track_id, :band_id
+    
+    def initialize(track)
+      @lyrics        = track['lyrics']
+      @downloadable  = track['downloadable']
+      @duration      = track['duration']
+      @about         = track['about']
+      @album_id      = track['album_id']
+      @credits       = track['credits']
+      @streaming_url = track['streaming_url']
+      @number        = track['number']
+      @title         = track['title']
+      @url           = track['url']
+      @track_id      = track['track_id']
+      @band_id       = track['band_id']
+    end
+    
+    def album
+      @album ||= Album.load(album_id)
+    end
+    
+    def band
+      @band ||= Band.load(band_id)
+    end
+    
+    class << self
+      def load(id)
+        response = get("/track/1/info", :query => { :key => Bandcamp::Base.api_key, :track_id => id })
+        new(response) if response
+      end
+    end
   end
 end
--- test/bandcamp_test.rb
+++ test/bandcamp_test.rb
@@ -18,10 +18,10 @@ class BandTest < Test::Unit::TestCase
   
   context "A band" do
     setup do      
-      stub_json_request("http://api.bandcamp.com/api/band/1/info?band_id=3463798201&key=SECRET_API_KEY", "band")
+      stub_json_request("http://api.bandcamp.com/api/band/3/info?band_id=3463798201&key=SECRET_API_KEY", "band")
       
       Bandcamp::Base.api_key = 'SECRET_API_KEY'
-      @band = Bandcamp::Band.new('3463798201')
+      @band = Bandcamp::Band.load('3463798201')
     end
 
     should "have a name" do
@@ -39,26 +39,33 @@ class BandTest < Test::Unit::TestCase
     should "have a subdomain" do
       assert_equal("amandapalmer", @band.subdomain)
     end
+    
+    should "have an offsite url" do
+      assert_equal("http://www.amandapalmer.net", @band.offsite_url)
+    end
+  end
   
-    context "discography" do
-      setup do
-        stub_json_request("http://api.bandcamp.com/api/band/1/discography?band_id=3463798201&key=SECRET_API_KEY", "band_discography")
-        
-        @discography = @band.discography
-      end
+  context "A band discography" do
+    setup do
+      stub_json_request("http://api.bandcamp.com/api/band/3/info?band_id=203035041&key=SECRET_API_KEY", "sufjanstevens")
+      stub_json_request("http://api.bandcamp.com/api/band/3/discography?band_id=203035041&key=SECRET_API_KEY", "band_discography")
+      
+      Bandcamp::Base.api_key = 'SECRET_API_KEY'
+      @band = Bandcamp::Band.load('203035041')
+      @discography = @band.discography
+    end
 
-      should "return all albums" do
-        assert_equal(11, @discography.length)
-      end
+    should "return all albums" do
+      assert_equal(9, @discography.length)
     end
   end
   
   context "An album" do
     setup do
-      stub_json_request("http://api.bandcamp.com/api/album/1/info?album_id=2587417518&key=SECRET_API_KEY", "album")
+      stub_json_request("http://api.bandcamp.com/api/album/2/info?album_id=2587417518&key=SECRET_API_KEY", "album")
         
       Bandcamp::Base.api_key = 'SECRET_API_KEY'
-      @album = Bandcamp::Album.new("2587417518")
+      @album = Bandcamp::Album.load("2587417518")
     end
     
     should "have a title" do
@@ -90,28 +97,133 @@ class BandTest < Test::Unit::TestCase
     end
     
     should "have a band" do
-      stub_json_request("http://api.bandcamp.com/api/band/1/info?band_id=3463798201&key=SECRET_API_KEY", "band")
-      
+      stub_json_request("http://api.bandcamp.com/api/band/3/info?band_id=3463798201&key=SECRET_API_KEY", "band")      
       assert_equal("Amanda Palmer", @album.band.name)
     end
-
-    # These methods aren't covered in the sample API calls Bandcamp provide .. 
-    # http://bandcamptech.wordpress.com/2010/05/15/bandcamp-api/
-    
-    # However they are mentioned in the API documentation so I've added extra data to the sample json files
-    # so they can be tested for coverage ..
     
     should "have any specific artist information" do
+      # this has been added to the fixture as the response from
+      # band camp doesn't include this information...
       assert_equal("John Finn", @album.artist)      
     end
     
     should "have a downloadable id" do
-      assert_equal(1, @album.downloadable)
+      assert_equal(2, @album.downloadable)
     end
     
     should "have a url" do
-      assert_equal("http://example.album.url", @album.url)
+      assert_equal("http://music.amandapalmer.net/album/who-killed-amanda-palmer", @album.url)
     end
   end
+  
+  context "A track" do
+    setup do
+      stub_json_request("http://api.bandcamp.com/api/track/1/info?track_id=1269403107&key=SECRET_API_KEY", "track")
+      Bandcamp::Base.api_key = 'SECRET_API_KEY'
+      
+      @track = Bandcamp::Track.load("1269403107")
+    end
+  
+    should "have a duration" do
+      assert_equal(317.378, @track.duration)
+    end
+    
+    should "have lyrics" do
+      assert_match(/When you were here before/, @track.lyrics)
+    end
+    
+    should "have an album" do
+      stub_json_request("http://api.bandcamp.com/api/album/2/info?album_id=1003295408&key=SECRET_API_KEY", "album")
+      assert_equal(1003295408, @track.album_id)
+      assert_equal("Who Killed Amanda Palmer", @track.album.title)
+    end
+    
+    should "have downloadable flag" do
+      assert_equal(2, @track.downloadable)
+    end
+    
+    should "have about" do
+      assert_match(/Recorded live in Prague/, @track.about)
+    end
+    
+    should "have track number" do
+      assert_equal(7, @track.number)
+    end
+    
+    should "have credits" do
+      assert_match(/Featuring: Amanda Palmer/, @track.credits)
+    end
+    
+    should "have a title" do
+      assert_equal("Creep (Live in Prague)", @track.title)
+    end
+    
+    should "have streaming url" do
+      assert_equal("http://popplers5.bandcamp.com/download/track?enc=mp3-128&id=1269403107&stream=1&ts=1298800847.0&tsig=67543bb7f276035915039c5545744d3b", @track.streaming_url)
+    end
+    
+    should "have track id" do
+      assert_equal(1269403107, @track.track_id)
+    end
+    
+    should "have a url" do
+      assert_equal("/track/creep-live-in-prague", @track.url)
+    end
+    
+    should "have a band" do
+      assert_equal(3463798201, @track.band_id)
+      stub_json_request("http://api.bandcamp.com/api/band/3/info?band_id=3463798201&key=SECRET_API_KEY", "band")      
+      assert_equal("Amanda Palmer", @track.band.name)
+    end
+  end
+   
+  context "searching" do
+    setup do
+      stub_json_request("http://api.bandcamp.com/api/band/3/search?key=SECRET_API_KEY&name=baron%20von%20luxxury", "search")
+      Bandcamp::Base.api_key = 'SECRET_API_KEY'
+      
+      @bands = Bandcamp::Band.find('baron von luxxury')
+    end
+  
+    should "return two bands" do
+      assert_equal(2, @bands.length)
+    end
+    
+    should "parse the bands correctly" do
+      assert_equal(1366501350, @bands[0].band_id)
+    end
+  end
+  
+  context "multiple bands" do
+    setup do
+      stub_json_request("http://api.bandcamp.com/api/band/3/info?key=SECRET_API_KEY&band_id=4214473200,3789714150", "multiple_bands")
+      Bandcamp::Base.api_key = 'SECRET_API_KEY'
+      
+      @bands = Bandcamp::Band.load('4214473200', '3789714150')
+    end
+  
+    should "return two bands" do
+      assert_equal(2, @bands.length)
+    end
+    
+    should "parse the bands correctly" do
+      assert_equal(4214473200, @bands[0].band_id)
+      assert_equal(3789714150, @bands[1].band_id)
+    end
+  end
+  
+  context "url search" do
+    setup do
+      stub_json_request("http://api.bandcamp.com/api/url/1/info?key=SECRET_API_KEY&url=http://music.sufjan.com/track/enchanting-ghost", "url_search")
+      Bandcamp::Base.api_key = 'SECRET_API_KEY'
+      @results = Bandcamp::Base.url('http://music.sufjan.com/track/enchanting-ghost')
+    end
+
+    should "return results" do
+      assert_equal(2323108455, @results['track_id'])
+      assert_equal(203035041, @results['band_id'])
+    end
+  end
+  
   
 end
